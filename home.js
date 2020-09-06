11f9f3be7849c81f7920dc68b6782339718beb74
gaffasaurus <christopher.yao.27@gmail.com>
2020-09-05 17:10:47 -0700

    update

--- home.js
+++ home.js
@@ -1,8 +1,7 @@
-function enterRoom() {
-  window.open("room/room.html", "_self");
-}
+let action;
 
 function inputInfo(action) {
+  action = action;
   const buttons = document.getElementsByClassName("room");
   for (let button of buttons) {
     button.style.display = "none";
@@ -22,9 +21,29 @@ function inputName() {
   nameField.style.display = "flex";
 }
 
+function validateId(id) {
+  const peer = new Peer();
+  peer.on('connection', conn => {
+    enterRoom();
+    return;
+  });
+  const p = document.getElementById("connecting");
+  p.style = "text-align: center";
+  p.innerHTML = "Connecting...";
+  idField.appendChild(p);
+  setTimeout(() => {
+    p.style = "text-align: center; color: red";
+    p.innerHTML = "Unable to establish connection, please check that your ID is correct."
+  }, 3000);
+}
+
+function enterRoom() {
+  window.open("room/room.html", "_self");
+}
+
 idField.addEventListener('keypress', e => {
   if (e.keyCode === 13) {
-    inputName();
+    validateId(idField.value);
   }
 });
 
--- room/room.js
+++ room/room.js
@@ -1,20 +1,40 @@
 const width = window.innerWidth;
 const height = window.innerHeight;
-console.log(width, height/1.5)
 
 const display = document.getElementsByClassName("display")[0];
 display.style += "; outline: 1.5px solid black; width: " + width/2.5 + "px; height: " + height/1.4 + "px; background-color: rgb(227, 238, 255);";
 
 const input = document.getElementById("input");
 
-const peer = new Peer();
 let roomId;
+const peer = new Peer();
+let connectedToServer = false;
 
-peer.on('open', id => {
-  roomId = id;
-  const h1 = document.getElementsByTagName("h1")[0];
-  h1.innerHTML = "ID: " + id;
-})
+function initializePeer() {
+  peer.on('open', id => {
+    roomId = id;
+    const h1 = document.getElementById("id");
+    h1.innerHTML = "ID: " + id;
+    console.log("Your ID: " + id);
+    connectedToServer = true;
+    connectToHost(id);
+  });
+}
+
+function connectToHost(id) {
+  for (let c of allConnections) {
+    c.close();
+  }
+  let conn = peer.connect(id);
+  conn.on('open', () => {
+    conn.on('data', data => {
+      let splitData = data.split(",");
+      console.log(splitData);
+    })
+  })
+}
+
+initializePeer();
 
 function sendMessage() {
   const text = input.value;
