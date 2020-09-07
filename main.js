01fb4e0e261bed09c0951404005b2e3533f1423f
gaffasaurus <christopher.yao.27@gmail.com>
2020-09-07 11:35:51 -0700

    connections enabled

--- main.js
+++ main.js
@@ -1,49 +1,45 @@
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-function enterRoom(action) {
-  const chatRoom = document.getElementById("chatroom");
-  const home = document.getElementById("home");
-  const messageBoard = document.getElementById("message-board");
-  const inputMessage = document.getElementById("input-message");
-  const messageBoardWidth = width/1.7;
+const chatRoom = document.getElementById("chatroom");
+const home = document.getElementById("home");
+const messageBoard = document.getElementById("message-board");
+const inputMessage = document.getElementById("input-message");
 
-  home.style.display = "none";
-  chatRoom.style.display = "flex";
-  messageBoard.style.width = messageBoardWidth + "px;";
-  messageBoard.style.height= height/1.4 + "px";
-
-  inputMessage.addEventListener("keypress", e => {
-    if (e.keyCode === 13) {
-      const value = inputMessage.value;
-      alert(value);
-      inputMessage.value = "";
-    }
-  });
-  if (action === "create") {
-    createRoom();
-  }
-}
 
 const joinRoomField = document.getElementById("join-room-field");
 joinRoomField.addEventListener("keypress", e => {
   if (e.keyCode === 13) {
     const value = joinRoomField.value;
-    alert(value);
+    joinRoom();
   }
 });
 
+const allConnections = [];
 const peer = new Peer();
 let conn;
-initializePeer();
 let peerId;
+initializePeer();
+
+function initializePeer() {
+  peer.on("open", id => {
+    console.log("My ID: " + id);
+    peerId = id;
+  });
+
+  peer.on("error", err => {
+    alert("" + err);
+  });
+}
+
 function createRoom() {
   console.log("room created");
   const displayId = document.getElementById("display-id");
   displayId.innerHTML = "ID: " + peerId;
   peer.on("connection", conn => {
-    alert("Received connection");
+    console.log("Received connection");
   });
+  updateAllConnections(peer);
 }
 
 function joinRoom() {
@@ -54,7 +50,54 @@ function validateId(id) {
   conn = peer.connect(id);
   conn.on('open', () => {
     console.log("connected!");
+    updateAllConnections(peer);
+    const displayId = document.getElementById("display-id");
+    displayId.innerHTML = "ID: " + conn.peer;
+    enterRoom('join');
   });
+}
+
+function enterRoom(action) {
+  const messageBoardWidth = width/1.7;
+
+  home.style.display = "none";
+  chatRoom.style.display = "flex";
+  messageBoard.style.width = messageBoardWidth + "px;";
+  messageBoard.style.height= height/1.4 + "px";
+
+  inputMessage.addEventListener("keypress", e => {
+    if (e.keyCode === 13) {
+      sendMessage();
+    }
+  });
+  if (action === "create") {
+    createRoom();
+  } else {
+  }
+}
+
+function updateAllConnections(peer) {
+  if (!allConnections.includes(peer)) {
+    allConnections.push(peer);
+  }
+}
+
+function sendMessage() {
+  const value = inputMessage.value;
+  console.log(allConnections);
+  for (let connection of allConnections) {
+    connection.send(value);
+  }
+  displayMessage(value);
+  inputMessage.value = "";
+}
+
+function displayMessage(text) {
+  const p = document.createElement("p");
+  const node = document.createTextNode(text);
+  p.appendChild(node);
+  messageBoard.appendChild(p);
+  messageBoard.scrollTo(0, messageBoard.scrollHeight);
 }
 
 // function validateId(id) {
@@ -72,14 +115,3 @@ function validateId(id) {
 //     joinMsg.style.color = "red";
 //   }, 5000);
 // }
-
-function initializePeer() {
-  peer.on("open", id => {
-    console.log("My ID: " + id);
-    peerId = id;
-  });
-
-  peer.on("error", err => {
-    alert("" + err);
-  });
-}
